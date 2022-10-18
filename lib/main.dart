5678065c7faf9580178310176113c2199553ae3c
safadtm <safadtm2004@gmail.com>
2022-10-18 16:42:22 +0530

    hbk

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }
@@ -58,6 +59,7 @@ class _HomePageState extends State<HomePage> {
                               fontWeight: FontWeight.bold,
                               color: Colors.white),
                         ),
+                        const SizedBox(height: 10),
                         Text(
                           xScore.toString(),
                           style: const TextStyle(
@@ -78,6 +80,7 @@ class _HomePageState extends State<HomePage> {
                               fontWeight: FontWeight.bold,
                               color: Colors.white),
                         ),
+                        const SizedBox(height: 10),
                         Text(
                           oScore.toString(),
                           style: const TextStyle(
@@ -99,16 +102,32 @@ class _HomePageState extends State<HomePage> {
                   crossAxisCount: 3),
               itemBuilder: (BuildContext context, int index) {
                 return GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white),
-                    ),
-                    child: Center(
-                      child: Text(
-                        displayElement[index],
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 35),
+                  onTap: () {
+                    _tapped(index);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        boxShadow: const [
+                          BoxShadow(
+                            blurRadius: 25.0,
+                          ),
+                        ],
+                      ),
+                      child: Container(
+                        color: Colors.white,
+                        child: Center(
+                          child: Text(
+                            displayElement[index],
+                            style: TextStyle(
+                                color: displayElement[index] == 'O'
+                                    ? Colors.red
+                                    : Colors.green,
+                                fontSize: 35),
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -116,8 +135,184 @@ class _HomePageState extends State<HomePage> {
               },
             ),
           ),
+          Expanded(
+            // Button for Clearing the Enter board
+            // as well as Scoreboard to start allover again
+
+            child: SizedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.indigo[50],
+                      foregroundColor: Colors.black,
+                    ),
+                    onPressed: _clearScoreBoard,
+                    child: const Text("Clear Score Board"),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+/////////////////////////////functions
+// filling the boxes when tapped with X
+// or O respectively and then checking the winner function
+  void _tapped(int index) {
+    setState(() {
+      if (oTurn && displayElement[index] == '') {
+        displayElement[index] = 'O';
+
+        filledBoxes++;
+      } else if (!oTurn && displayElement[index] == '') {
+        displayElement[index] = 'X';
+
+        filledBoxes++;
+      }
+      oTurn = !oTurn;
+      _checkWinner();
+    });
+  }
+
+  void _checkWinner() {
+    // Checking rows
+    if (displayElement[0] == displayElement[1] &&
+        displayElement[0] == displayElement[2] &&
+        displayElement[0] != '') {
+      //  _showWinDialog(displayElement[0]);
+      showWinSnackBar(displayElement[0]);
+    }
+    if (displayElement[3] == displayElement[4] &&
+        displayElement[3] == displayElement[5] &&
+        displayElement[3] != '') {
+      showWinSnackBar(displayElement[3]);
+    }
+    if (displayElement[6] == displayElement[7] &&
+        displayElement[6] == displayElement[8] &&
+        displayElement[6] != '') {
+      showWinSnackBar(displayElement[6]);
+    }
+
+    // Checking Column
+    if (displayElement[0] == displayElement[3] &&
+        displayElement[0] == displayElement[6] &&
+        displayElement[0] != '') {
+      showWinSnackBar(displayElement[0]);
+    }
+    if (displayElement[1] == displayElement[4] &&
+        displayElement[1] == displayElement[7] &&
+        displayElement[1] != '') {
+      showWinSnackBar(displayElement[1]);
+    }
+    if (displayElement[2] == displayElement[5] &&
+        displayElement[2] == displayElement[8] &&
+        displayElement[2] != '') {
+      showWinSnackBar(displayElement[2]);
+    }
+
+    // Checking Diagonal
+    if (displayElement[0] == displayElement[4] &&
+        displayElement[0] == displayElement[8] &&
+        displayElement[0] != '') {
+      showWinSnackBar(displayElement[0]);
+    }
+    if (displayElement[2] == displayElement[4] &&
+        displayElement[2] == displayElement[6] &&
+        displayElement[2] != '') {
+      showWinSnackBar(displayElement[2]);
+    } else if (filledBoxes == 9) {
+      showDrawSnackBar();
+    }
+  }
+
+  void showWinSnackBar(String winner) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 10),
+        content: Container(
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 75),
+          decoration: BoxDecoration(
+              border: Border.all(width: 2.0, color: Colors.black),
+              borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "\" $winner \" is Winner!!!",
+              style: const TextStyle(fontSize: 25),
+            ),
+          ),
+        ),
+        backgroundColor: Colors.black,
+        elevation: 1000,
+        behavior: SnackBarBehavior.floating,
+        action: SnackBarAction(
+          label: 'Play again',
+          onPressed: () {
+            _clearBoard();
+          },
+        ),
+      ),
+    );
+    if (winner == 'O') {
+      oScore++;
+    } else if (winner == 'X') {
+      xScore++;
+    }
+  }
+
+  void showDrawSnackBar() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 10),
+        content: Container(
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 75),
+          decoration: BoxDecoration(
+              border: Border.all(width: 2.0, color: Colors.black),
+              borderRadius: BorderRadius.circular(20)),
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Draw",
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ),
+        backgroundColor: Colors.black,
+        elevation: 1000,
+        behavior: SnackBarBehavior.floating,
+        action: SnackBarAction(
+          label: 'Play again',
+          onPressed: () {
+            _clearBoard();
+          },
+        ),
+      ),
+    );
+  }
+
+  void _clearBoard() {
+    setState(() {
+      for (int i = 0; i < 9; i++) {
+        displayElement[i] = '';
+      }
+    });
+
+    filledBoxes = 0;
+  }
+
+  void _clearScoreBoard() {
+    setState(() {
+      xScore = 0;
+      oScore = 0;
+      for (int i = 0; i < 9; i++) {
+        displayElement[i] = '';
+      }
+    });
+    filledBoxes = 0;
   }
 }
